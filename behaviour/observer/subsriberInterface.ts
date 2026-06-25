cc4f1b33fafd2baca61fba07c85bb946e7ed242f
Roman Tymchenko <roman.tymchenko@valtech.com>
2026-06-25 10:11:54 +0200

    fix peace of some code

--- behaviour/observer/subsriberInterface.ts
+++ behaviour/observer/subsriberInterface.ts
@@ -1,3 +1,3 @@
 export interface ISubscriber {
     update(videoTitle: string): void;
-  }
+}
--- creational/abstractFactory/abstractFactory.ts
+++ creational/abstractFactory/abstractFactory.ts
@@ -1,8 +1,7 @@
-import { Car } from "./car";
-import { Engine } from "./engine";
+import Car from "./car.ts";
+import Engine from "./engine.ts";
 
 export abstract class AbstractFactory {
     abstract createCar(name: string, speed: number): Car;
     abstract createEngine(name: string, resource: number): Engine;
 }
-
